02fbc4250b370044ac95d09b825ba10082bde352
jatinjain22 <jatin@Jatin-Mac.local>
2016-03-20 16:01:48 +0530

    3

--- app/controllers/api/service_requests_controller.rb
+++ app/controllers/api/service_requests_controller.rb
@@ -44,7 +44,7 @@ class Api::ServiceRequestsController < ApplicationController
                
                  @userid=params[:user_id]
                 
-         @request = ServiceRequest.where(["status=? and user_id=?","Completed",@userid]).order('service_requests.date DESC').order('service_requests.request_time DESC').first
+         @request = ServiceRequest.where(user_id: params[:user_id]).where(status: "Completed").order('service_requests.date DESC').order('service_requests.request_time DESC').first
          
         render json: {
                  status: 'success',
@@ -54,9 +54,11 @@ class Api::ServiceRequestsController < ApplicationController
 
     def show
            @service_requests = ServiceRequest.where(user_id: params[:user_id])
+            @request = ServiceRequest.where(user_id: params[:user_id]).where(status: "Completed").order('service_requests.date DESC').order('service_requests.request_time DESC').first
+        
     render json: {
                status: 'success',
-               data: @service_requests
+               data: @request
                #user_id: user_id
            }, status: 200
     end 
